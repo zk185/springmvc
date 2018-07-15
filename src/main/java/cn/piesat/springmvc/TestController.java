17e1f58f9c2429107548c1bd29526025ce009340
zhaokai <506683059@qq.com>
2018-07-15 21:54:32 +0800

    只搭建springmvc版本V0.1

---
+++ src/main/java/cn/piesat/springmvc/TestController.java
@@ -0,0 +1,14 @@
+package cn.piesat.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/testMathod")
+    public String test(){
+        return "1";
+    }
+}
